9b95dea6de22a7bfa3a9f1be8b7d171742e02ccd
keitheous <keithchongwy@gmail.com>
2016-05-10 23:56:37 +1000

    refactored some

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,17 @@
 class SessionsController < ApplicationController
 
-
   def index
-
   end
 
   def signup
-    
   end
 
   def login
-
   end
 
   def join
     if User.find_by(name: params[:name]) || !Country.find_by(name: params[:country])
-      redirect_to '/'
+      redirect_to '/signup'
     else
       user = User.new
       user.name = params[:name].downcase
@@ -31,16 +27,15 @@ class SessionsController < ApplicationController
     user = User.find_by(name: "#{params[:username].downcase}" )
       if user && user.authenticate(params[:password])
         session[:id] = user.id
-        redirect_to "/profile"
+        redirect_to '/profile'
       else
-        session[:attempt] = true
+        redirect_to '/login'
       end
-
   end
 
 
   def logout
-    session[:id] = nil    
+    session[:id] = nil
     redirect_to "/"
   end
 
