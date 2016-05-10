794711eb22409e284d76c9883009ede01b4e4a9f
LJ Kenward <ljkrooker@gmail.com>
2016-05-10 11:14:07 +1000

    new card creation form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+         #controller #method
   get '/' => 'cards#index'
   get '/signup' => 'cards#signup'
   get '/login' => 'cards#login'
+  #new card page
   get '/new' => 'cards#new'
+  #new card form creationaw
+  post '/cards' => 'cards#create'
+
 end
