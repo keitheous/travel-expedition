baacb72b39f7f65dee9b414e9846c7ff63b73748
Emily Bie <bieyuyu@gmail.com>
2016-05-10 11:40:40 +1000

    user profile by emily

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,14 @@
 class CardsController < ApplicationController
+
   def index
+    @user = User.first
+    @cards = @user.cards
+    @countries = []
+    @cards.each do |card|
+      @countries.push(card.countries)
+    end
+    @countries.join(",")
+    @countries.uniq!
 
   end
 
