17216d3a32e10f9daeac87406a887a592c1afbfb
keitheous <keithchongwy@gmail.com>
2016-05-11 16:24:02 +1000

    API WOrking

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name:"Wolf",password:"apple", default_currency:"Australia");
-User.create(name:"Emily",password:"apple", default_currency:"Australia");
-User.create(name:"LJ",password:"apple", default_currency:"Australia");
-User.create(name:"Keithy",password:"apple", default_currency:"Australia");
+User.create(name:"Wolf",password:"apple", country:"Australia");
+User.create(name:"Emily",password:"apple", country:"Australia");
+User.create(name:"LJ",password:"apple", country:"Australia");
+User.create(name:"Keithy",password:"apple", country:"Australia");
 Card.create(title:"test0",user_id:1);
 Card.create(title:"test1",user_id:1);
 Card.create(title:"mountain",user_id:1);
@@ -285,6 +285,6 @@ CardTag.create(card_id: 4, tag_id:4);
 
 #   newTag = Tag.new
 #   newTag.name = tag
-#   newTag.save 
+#   newTag.save
 
 # end
