33fc21e48686c66c3d88e554d3a4715d8d21e9a3
keitheous <keithchongwy@gmail.com>
2016-05-11 16:02:24 +1000

    ajax

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,100 +1,21 @@
-class CardsController < ApplicationController
+module Api
+ class CardsController < ApplicationController
 
-  def index
+   def board
+     @cards = Card.all
+     render json: @cards.to_json
+   end
 
-  end
+   def create
+     card = Card.new
+     card.title = params[:title]
+     card.image = params[:image]
+     card.source = params[:source]
 
-  def profile
-    @user = User.find(session[:id])
-    @cards = @user.cards
-    # get all the cards.countries belongs to the user
-    @countries = []
-
-    @cards.each do |card|
-      card.countries.each do |country|
-        if @countries.index(country) == nil
-          @countries.push(country)
-        end
-      end
-    end
-
-  end
-
-  def show
-    @tag = Tag.find(params[:id])
-    @user = User.find(session[:id])
-    @cards = @tag.cards & @user.cards
-  end
-
-  def signup
-
-  end
-
-  def login
-
-  end
-
-  def new
-
-  end
-
-
-  def board
-    @user = User.find(session[:id])
-    @tags = Tag.all
-    @country_id = params[:country_id]
-    @country = Country.find(@country_id)
-    # displaying common cards between @country.cards(ALL) and user's card by using the & method
-    @cards = @country.cards & @user.cards
-    # currency exchange api parameters
-    default_country = @user.default_currency
-    dest_country = @country.name
-    @base = IsoCountryCodes.search_by_name(default_country).first.currency
-    @dest = IsoCountryCodes.search_by_name(dest_country).first.currency
-
-  end
-
-  def create
-
-    card = Card.new
-    card.title = params[:title]
-    card.image = params[:image]
-    card.source = params[:source]
-    card.user_id = session[:id]
-
-    country = Country.find_by(name: params[:country])
-    card.countries << country
-    card.save
-
-    tags = [
-      [:foodanddrink, "Food & Drink"],
-      [:accommodation, "Accommodation"],
-      [:transport, "Transport"],
-      [:culture, "Culture"],
-      [:entertainment, "Entertainment"],
-      [:shopping, "Shopping"],
-      [:nature, "Nature"],
-      [:free, "Free"]
-    ]
-
-    tags.each do |tag|
-
-        if params[tag[0]] === 'on'
-
-          cardtag = CardTag.new
-          tag_data = Tag.find_by name: tag[1]
-          cardtag.tag_id = tag_data.id
-          cardtag.card_id = card.id
-          cardtag.save
-
-        else
-          puts "not on"
-        end
-
-    end
-
-    redirect_to '/profile'
-
-    end
+     if card.save
+       render json: card.to_json, status: 201
+     else
+       render json: card.errors.to_json
+     end
 
 end
