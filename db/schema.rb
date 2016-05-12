665cc656e2bda54baf2772f4f8b176448e766752
Emily Bie <bieyuyu@gmail.com>
2016-05-12 20:05:43 +1000

    emergency numbers in board

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511041045) do
+ActiveRecord::Schema.define(version: 20160512013017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160511041045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.boolean  "complete"
   end
 
   create_table "countries", force: :cascade do |t|
