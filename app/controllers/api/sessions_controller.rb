66ac8cb5c09c6c954457c90d563c0dc5e315a78f
Wolf <contact@nickwolf.com.au>
2016-05-12 10:30:47 +1000

    changed food and drink to food

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,22 +1,22 @@
 module Api
-class SessionsController < ApplicationController
+  class SessionsController < ApplicationController
 
-  def signup
-    if User.find_by(name: params[:name]) || !Country.find_by(name: params[:country])
-      redirect_to '/signup'
-    else
-      @user = User.new
-      @user.name = params[:name].downcase
-      @user.email =params[:email]
-      @user.password = params[:password]
-      @user.country = params[:country]
-      @user.save
-      session[:id] = user.id
-      render json: @user.to_json, status: 201
+    def signup
+      if User.find_by(name: params[:name]) || !Country.find_by(name: params[:country])
+        redirect_to '/signup'
+      else
+        @user = User.new
+        @user.name = params[:name].downcase
+        @user.email =params[:email]
+        @user.password = params[:password]
+        @user.country = params[:country]
+        @user.save
+        session[:id] = user.id
+        render json: @user.to_json, status: 201
+      end
     end
+
+
   end
-
-
-end
 
 end
